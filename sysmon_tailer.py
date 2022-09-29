4d352636e22e384e86b13ffb02c558534e87c939
uahmad417 <18u.ahmad@gmail.com>
2022-09-29 16:43:41 +0500

    added sysmon_tailer sysmon_tailer tails for sysmon logs and converts the logs to json

---
+++ sysmon_tailer.py
@@ -0,0 +1,25 @@
+import json
+import xmltodict
+import Evtx.Evtx as evtx
+import configparser
+
+def readconfig():
+    global config
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+def evtx_to_json():
+    with evtx.Evtx(config['sysmon']['path']) as open_logs:
+        events = list(open_logs.records)
+        count = len(events)
+        for record in events:
+            json_log = json.loads(json.dumps(xmltodict.parse(record.xml())))
+            event_data = json_log['Event']['EventData']['Data']
+            for attribute in event_data:
+                attribute[attribute.pop('@Name')] = attribute.pop('#text')
+            with open('sysmon_logs.json','a') as sysmon_logs:
+                sysmon_logs.write(json.dumps(json_log)+'\n')
+
+if __name__ == '__main__':
+    readconfig()
+    evtx_to_json()
