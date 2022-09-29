fead602c6f93adf1765131424d52480d37afdb64
uahmad417 <18u.ahmad@gmail.com>
2022-09-29 16:02:44 +0500

    resolved Runtime error now scappy logs entire HTTP request infromation along with timestamps

--- scapy_monitor.py
+++ scapy_monitor.py
@@ -1,3 +1,4 @@
+import http
 from scapy.all import *
 from scapy.layers.http import HTTPRequest
 import json
@@ -8,27 +9,27 @@ def process_packet(packet):
     if packet.haslayer(HTTPRequest):
         for key in http_data:
             http_data[key] = ''
-        http_data['SourceIP'] = packet[IP].src
-        http_data['SourcePort']=packet[IP].sport
         for header in http_data:
-            print(header)
-            if header=='SourceIP' or header=='SourcePort':
+            if header == 'SourceIP':
+                http_data['SourceIP'] = packet[IP].src
+                continue
+            if header == 'SourcePort':
+                http_data['SourcePort'] = packet[IP].sport
                 continue
             if header == 'UtcTime':
                 http_data['UtcTime'] = str(datetime.now())
-                continue
+                break
             value = 'packet[HTTPRequest].'+header
             if eval(value) is not None:
-                command = 'packet[HTTPRequest].'+header+'.decode("utf-8")'
+                command = value+'.decode("utf-8")'
                 http_data[header] = eval(command)
-            if packet.haslayer(Raw):
-                http_data['Payload'] = packet[Raw].load.decode('utf-8')
-        with open('test.json', 'a') as scapy_logs:
+        if packet.haslayer(Raw):
+            http_data['Payload'] = packet[Raw].load.decode()
+        with open('scapy_logs.json','a') as scapy_logs:
             scapy_logs.write(json.dumps(http_data)+'\n')
-        print(json.dumps(http_data))
-
+        print(http_data)
 
 if __name__ == '__main__':
     global http_data
-    http_data = {'SourceIP': '','SourcePort': '', 'Method': '', 'Path': '', 'Http_Version': '' ,'Accept': '', 'Accept_Encoding': '', 'Accept_Language': '', 'Cache_Control': '',  'Connection':'', 'Content_Length': '','Content_Type': '', 'Cookie': '','Host':'', 'Origin': '', 'Referer': '', 'Upgrade_Insecure_Requests': '','User_Agent':'', 'UtcTime':''}
+    http_data = {'SourceIP': '','SourcePort': '', 'Method': '', 'Path': '', 'Http_Version': '' ,'Accept': '', 'Accept_Encoding': '', 'Accept_Language': '', 'Cache_Control': '',  'Connection':'', 'Content_Length': '','Content_Type': '', 'Cookie': '','Host':'', 'Origin': '', 'Referer': '', 'Upgrade_Insecure_Requests': '','User_Agent':'', 'UtcTime':'', 'Payload':''}
     sniff(iface='Wi-Fi',prn=process_packet)
