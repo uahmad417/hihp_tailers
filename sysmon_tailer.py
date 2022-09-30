4c04079307d1eaf073b2924e8292dc7f25eab581
uahmad417 <18u.ahmad@gmail.com>
2022-09-30 12:03:00 +0500

    convert utc to local timezone added datetime conversion to convert timestamps in sysmon logs from utc to local

--- sysmon_tailer.py
+++ sysmon_tailer.py
@@ -1,7 +1,10 @@
 import json
+from threading import local
 import xmltodict
 import Evtx.Evtx as evtx
 import configparser
+from datetime import datetime
+from dateutil import tz
 
 def readconfig():
     global config
@@ -9,6 +12,8 @@ def readconfig():
     config.read('config.ini')
 
 def evtx_to_json():
+    local_tz = tz.tzlocal()
+    utc_tz = tz.tz.gettz('UTC')
     with evtx.Evtx(config['sysmon']['path']) as open_logs:
         events = list(open_logs.records())
         count = len(events)
@@ -16,6 +21,12 @@ def evtx_to_json():
             json_log = json.loads(json.dumps(xmltodict.parse(record.xml())))
             event_data = json_log['Event']['EventData']['Data']
             for attribute in event_data:
+                if attribute['@Name'] == 'UtcTime':
+                    utc_timestamp = datetime.strptime(attribute['#text'],'%Y-%m-%d %H:%M:%S.%f')
+                    utc_timestamp = utc_timestamp.replace(tzinfo=utc_tz)
+                    local_timestamp = utc_timestamp.astimezone(local_tz)
+                    local_timestamp = local_timestamp.replace(tzinfo=None)
+                    attribute['#text'] = str(local_timestamp)
                 attribute[attribute.pop('@Name')] = attribute.pop('#text')
             with open('sysmon_logs.json','a') as sysmon_logs:
                 sysmon_logs.write(json.dumps(json_log)+'\n')
